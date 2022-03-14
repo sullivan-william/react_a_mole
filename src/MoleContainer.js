d4f7e7e38cb4e2eb0378bc80b2e35ae7177bd546
William Sullivan <sullivanwp95@gmail.com>
2022-03-14 14:42:17 -0400

    adding MoleContainer behavior

--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -1,12 +1,19 @@
 import Mole from "./Mole"
 import EmptySlot from "./EmptySlot"
+import { useState } from 'react'
 
 function MoleContainer(props) {
+    let [displayMole, setDisplayMole] = useState(false)
+
+    const hitMole = () => {
+        props.setScore(props.score + 1)
+        setDisplayMole(false)
+    }
+
     return (
         <div>
-            <h2>Mole Container</h2>
-            <Mole />
-            <EmptySlot />
+            <h2 onClick={() => displayMole ? hitMole() : null}>Mole Container</h2>
+            {displayMole ? <Mole /> : <EmptySlot />}
         </div>
     )
 }
