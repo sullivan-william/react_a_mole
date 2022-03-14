7f7fce88f71ece502725c7d7012dc2d82690c974
William Sullivan <sullivanwp95@gmail.com>
2022-03-14 13:37:55 -0400

    generating mole hills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import MoleContainer from './MoleContainer';
+import { useState } from 'react'
 
 function App() {
+let [score, setScore] = useState(0)
+
+const createMoleHill = () => {
+  let hills = []
+  for (let i = 0; i < 9; i++) {
+    hills.push(
+      <MoleContainer key={i} score={score} setScore={setScore}/>
+    )
+  }
+  return (
+    <div>
+      {hills}
+    </div>
+  )
+}
+
   return (
     <div className="App">
-      <MoleContainer />
+      <h1>React-a-Mole</h1>
+      {score}
+      {createMoleHill()}
     </div>
   );
 }
